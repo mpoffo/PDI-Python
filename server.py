aeeb7a4c182424371c83f02ff848f347b61ef4e2
Marcio <marcio.poffo@senior.com.br>
2019-10-24 15:11:00 -0300

    Testes ainda não funcional

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request as req, render_template_string, redirect, url_for
 from flask_restful import Resource, Api
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
+import http
+import requests
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,5 +19,22 @@ class Contacts(Resource):
 
 api.add_resource(Contacts, '/contacts')  # Route_1
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if req.method == 'POST':
+        if req.form['username'] != 'admin' or req.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+
+    r = requests.get('http://localhost:4200/#/login')
+    if r.status_code in [http.HTTPStatus.OK, http.HTTPStatus.CREATED, http.HTTPStatus.ACCEPTED]:
+        return r.content
+    else:
+        return r.content
+
+
 if __name__ == '__main__':
-    app.run(port=5002)
+    app.run(port=5002, debug=True)
